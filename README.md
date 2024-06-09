15ac51e18a7049a22cf8ab1b6c0576f543294b24
Kieran <leobot1010@gmail.com>
2024-06-09 17:46:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Project Description
+
+This is an app that creates PDF invoices out of Excel Files.
+
+# Notes
+
+- openpyxl module need to be installed for pandas to be able to import excel files
---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import glob
+
+filepaths = glob.glob("invoices/*.xlsx")   # imports all .xlsx files in a list
+
+for filepath in filepaths:
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    print(df)
