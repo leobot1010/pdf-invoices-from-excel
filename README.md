d356005ca500eb9c1e50fb7f7be1344dc25ef74b
Kieran <leobot1010@gmail.com>
2024-06-11 15:25:51 +0100

    Added the header row and the products rows

--- README.md
+++ README.md
@@ -5,3 +5,16 @@ This is an app that creates PDF invoices out of Excel Files.
 # Notes
 
 - openpyxl module need to be installed for pandas to be able to import excel files
+
+# Errors
+
+AttributeError: 'int' object has no attribute 'replace'. <br/>
+This occurred when first attempting to write some of the invoice data to pdf. <br/>
+The product_id column as well as other columns contain information as integers but <br/>
+the pdf format only accepts strings. 
+
+So I converted all cells to strings EG: <br/>
+
+pdf.cell(w=30, h=8, txt=row["product_id"])  <br/>
+to   <br/>
+pdf.cell(w=30, h=8, txt=str(row["product_id"]))   <br/>
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ import glob
 from fpdf import FPDF
 from pathlib import Path
 
-filepaths = glob.glob("invoices/*.xlsx")   # imports all .xlsx files in a list
+filepaths = glob.glob("invoices/*.xlsx")        # imports all .xlsx files in a list
 
-# a new pdf in created in each iteration
+# A NEW PDF IS CREATES IN EACH ITERATION
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
     filename = Path(filepath).stem
     invoice_no, the_date = filename.split("-")                      # double variable created in order [0] and [1]
@@ -19,5 +18,29 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice no: {invoice_no}", ln=1)      # add invoice number to pdf
     pdf.cell(w=50, h=8, txt=f"Date {the_date}", ln=1)               # add date to pdf
 
-    pdf.output(f"PDFs/Invoice_no_{invoice_no}.pdf")                 # you must manually create the PDFs folder
+    # READ IN THE EXCEL DATA
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
 
+    # ADD THE HEADER OF THE INVOICE
+    columns = list(df.columns)      # list(df.columns) returns a list of all the values from row 1 of Excel document
+    columns = [i.replace('_', ' ').title() for i in columns]   # removes the _ and capitalizes each word
+
+    pdf.set_font(family="Times", size=10, style='B')
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=30, h=8, txt=columns[4], border=1, ln=1)
+
+    # ADD THE PRODUCT ROW
+    for index, columns in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(columns["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(columns["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(columns["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(columns["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(columns["total_price"]), border=1, ln=1)   # ** ln=1 drops to new line for next item
+
+    pdf.output(f"PDFs/Invoice_no_{invoice_no}.pdf")                 # NOTE: you must manually create the PDFs folder
